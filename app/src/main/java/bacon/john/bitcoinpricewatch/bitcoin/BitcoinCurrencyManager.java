baf2b91e69f21b0e7f9eb6e16db522edd753938e
baconja2 <baconja2@gmail.com>
2016-02-26 12:39:46 -0800

    fleshing out the manager and service - the list of currencies will be populated by calling https://api.bitcoinaverage.com/ticker/global/, and the specific price data for each currency will be fetched by https://api.bitcoinaverage.com/ticker/global/some_currency_code

--- app/src/main/java/bacon/john/bitcoinpricewatch/bitcoin/BitcoinCurrencyManager.java
+++ app/src/main/java/bacon/john/bitcoinpricewatch/bitcoin/BitcoinCurrencyManager.java
@@ -1,5 +1,8 @@
 package bacon.john.bitcoinpricewatch.bitcoin;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import bacon.john.bitcoinpricewatch.localstorage.LocalStorageService;
@@ -10,6 +13,13 @@ import bacon.john.bitcoinpricewatch.localstorage.LocalStorageService;
  * Retrieves information about bitcoin in various currencies
  */
 public class BitcoinCurrencyManager {
+    public interface CurrencyListCallback {
+        public void callback(List<BitcoinCurrencyModel> currencies, Exception error);
+    }
+    public interface CurrencyCallback {
+        public void callback(BitcoinCurrencyModel currency, Exception error);
+    }
+
     private static BitcoinCurrencyManager sharedInstance;
 
     public static BitcoinCurrencyManager sharedInstance() {
@@ -21,5 +31,64 @@ public class BitcoinCurrencyManager {
 
     public List<BitcoinCurrencyModel> getCachedCurrencies() {
         return LocalStorageService.sharedInstance().getRealm().where(BitcoinCurrencyModel.class).findAll();
+    }
+
+    public BitcoinCurrencyModel getCachedCurrency(String currencyCode) {
+        return LocalStorageService.sharedInstance().getRealm().
+                where(BitcoinCurrencyModel.class).
+                equalTo("code", currencyCode).findFirst();
+    }
+
+    public void getRemoteCurrencyCodes(final CurrencyListCallback callback) {
+        BitcoinCurrencyService.sharedInstance().fetchCurrencyCodes(new BitcoinCurrencyService.Callback() {
+            @Override
+            public void callback(JSONObject response, Exception error) {
+                if (error != null) {
+                    callback.callback(null, error);
+                    return;
+                } else {
+                    callback.callback(BitcoinCurrencyManager.modelsFromJSON(response), null);
+                }
+            }
+        });
+    }
+
+    public void getRemoteCurrencyPrices(BitcoinCurrencyModel currency, final CurrencyCallback callback) {
+        BitcoinCurrencyService.sharedInstance().fetchCurrencyPrices(currency.getCode(), new BitcoinCurrencyService.Callback() {
+            @Override
+            public void callback(JSONObject response, Exception error) {
+                if (error != null) {
+                    callback.callback(null, error);
+                    return;
+                } else {
+                    callback.callback(BitcoinCurrencyManager.modelFromJSON(response), null);
+                }
+            }
+        });
+    }
+
+    // Parsing methods
+    private static List<BitcoinCurrencyModel> modelsFromJSON(JSONObject json) {
+        List<BitcoinCurrencyModel> models = new ArrayList<BitcoinCurrencyModel>();
+
+        // faking it for now
+        BitcoinCurrencyModel model = new BitcoinCurrencyModel();
+        model.setCode("USD");
+
+        models.add(model);
+
+        BitcoinCurrencyModel model2 = new BitcoinCurrencyModel();
+        model2.setCode("XYZ");
+
+        models.add(model2);
+
+        return models;
+    }
+    private static BitcoinCurrencyModel modelFromJSON(JSONObject json) {
+        // faking it for now
+        BitcoinCurrencyModel model = new BitcoinCurrencyModel();
+        model.setCode("USD");
+
+        return model;
     }
 }
--- app/src/main/java/bacon/john/bitcoinpricewatch/bitcoin/BitcoinCurrencyService.java
+++ app/src/main/java/bacon/john/bitcoinpricewatch/bitcoin/BitcoinCurrencyService.java
@@ -2,6 +2,10 @@ package bacon.john.bitcoinpricewatch.bitcoin;
 
 import com.android.volley.*;
 import com.android.volley.toolbox.*;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @class BitcoinCurrencyService
@@ -9,14 +13,25 @@ import com.android.volley.toolbox.*;
  * Fetches data about bitcoin in various currencies
  */
 public class BitcoinCurrencyService {
+    public interface Callback {
+        public void callback(JSONObject response, Exception error);
+    }
+
     private static BitcoinCurrencyService sharedInstance;
+
+    private static final String CURRENCY_CODE_BASE_URL = "https://api.bitcoinaverage.com/ticker/global/";
+
     private RequestQueue requestQueue;
+
+    private Map<String,String> codeMap;
 
     private BitcoinCurrencyService() {
         Cache cache = new NoCache();
         Network network = new BasicNetwork(new HurlStack());
         requestQueue = new RequestQueue(cache, network);
         requestQueue.start();
+
+        codeMap = new HashMap<String,String>();
     }
 
     public static BitcoinCurrencyService sharedInstance() {
@@ -24,5 +39,40 @@ public class BitcoinCurrencyService {
             sharedInstance = new BitcoinCurrencyService();
         }
         return sharedInstance;
+    }
+
+    public void fetchCurrencyCodes(final Callback callback) {
+        JsonObjectRequest req = new JsonObjectRequest(
+                Request.Method.GET, CURRENCY_CODE_BASE_URL, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        callback.callback(response, null);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        callback.callback(null, error);
+                    }
+                });
+        requestQueue.add(req);
+    }
+
+    public void fetchCurrencyPrices(String code, final Callback callback) {
+        if (code == null) {
+            throw new AssertionError("currency code must not be null");
+        }
+        JsonObjectRequest req = new JsonObjectRequest(
+                Request.Method.GET, CURRENCY_CODE_BASE_URL + code, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                callback.callback(response, null);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                callback.callback(null, error);
+            }
+        });
+        requestQueue.add(req);
     }
 }
